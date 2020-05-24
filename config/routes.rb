65015bad2191f4474b180f5ae199d6296656c690
nabe-nabe <w_nabe_shinya@yahoo.co.jp>
2020-05-24 19:15:08 +0900

    review機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :reviews, only: [:index, :destroy]
   root 'books#index'
-  resources :books
+  resources :books do
+    resources :reviews, except: [:index, :destroy]
+  end
   devise_for :users
 end
